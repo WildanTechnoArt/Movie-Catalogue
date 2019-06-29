f04a661018921c4254636ffbba21c66cb2c87001
Wildan M Athoillah <wildantechnoart77@gmail.com>
2019-06-30 02:08:42 +0700

    Add Movie and TV Show Navigation Add Language Change Use Material Design UI

--- app/src/main/java/com/wildan/moviecatalogue/activity/MainActivity.kt
+++ app/src/main/java/com/wildan/moviecatalogue/activity/MainActivity.kt
@@ -1,55 +1,56 @@
 package com.wildan.moviecatalogue.activity
 
-import android.content.res.TypedArray
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.wildan.moviecatalogue.model.Movie
-import com.wildan.moviecatalogue.adapter.MovieAdapter
+import android.provider.Settings
+import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
+import android.view.Menu
+import android.view.MenuItem
 import com.wildan.moviecatalogue.R
+import com.wildan.moviecatalogue.fragment.*
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
-
-    private var listTitle: Array<String>? = null
-    private var listDate: Array<String>? = null
-    private var listRating: Array<String>? = null
-    private var listGenres: Array<String>? = null
-    private var listDescription: Array<String>? = null
-    private lateinit var listPoster: TypedArray
-    private var movies: ArrayList<Movie>? = null
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        prepare()
-        addItem()
-
-        val adapter = MovieAdapter(this, movies)
-        lv_movie.adapter = adapter
+        setSupportActionBar(toolbar)
+        bottom_navigation.setOnNavigationItemSelectedListener(this)
+        loadFragment(MovieFragment())
     }
 
-    private fun prepare() {
-        listTitle = resources.getStringArray(R.array.data_title)
-        listDate = resources.getStringArray(R.array.data_date)
-        listRating = resources.getStringArray(R.array.data_rating)
-        listGenres = resources.getStringArray(R.array.data_genres)
-        listDescription = resources.getStringArray(R.array.data_description)
-        listPoster = resources.obtainTypedArray(R.array.data_poster)
-    }
-
-    private fun addItem() {
-        movies = ArrayList()
-
-        for (i in 0 until (listTitle?.size ?: 0)) {
-            val movie = Movie()
-            movie.poster = listPoster.getResourceId(i, -1)
-            movie.title = listTitle?.get(i).toString()
-            movie.date = listDate?.get(i).toString()
-            movie.rating = listRating?.get(i).toString()
-            movie.genres = listGenres?.get(i).toString()
-            movie.description = listDescription?.get(i).toString()
-            movies?.add(movie)
+    private fun loadFragment(fragment: Fragment?): Boolean {
+        if (fragment != null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.content_view, fragment)
+                .commit()
+            return true
         }
+        return false
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.activity_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.setting -> startActivity(Intent(Settings.ACTION_LOCALE_SETTINGS))
+        }
+        return true
+    }
+
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        var fragment: Fragment? = null
+
+        when (p0.itemId) {
+            R.id.movie_menu -> fragment = MovieFragment()
+            R.id.tvshow_menu -> fragment = TvShowFragment()
+        }
+        return loadFragment(fragment)
     }
 }
